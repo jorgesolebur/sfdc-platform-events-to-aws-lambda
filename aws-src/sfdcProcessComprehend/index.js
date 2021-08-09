67df39cb79667237a1b7642792817f519b78fd0a
Jorge <31279773+jorgesolebur@users.noreply.github.com>
2021-08-10 07:57:03 +0800

    Updated nodejx for compatibility
    compatible with v52.0

--- aws-src/sfdcProcessComprehend/index.js
+++ aws-src/sfdcProcessComprehend/index.js
@@ -19,7 +19,7 @@ exports.handler = function(event, context, callback) {
 
         // AWS Comprehend - Detect Dominant Language
         var dominantLangParams = {
-          Text: payload.Message__c
+          Text: payload.detail.Message__c
         };
         var dominantLangPromise = comprehend.detectDominantLanguage(dominantLangParams).promise();
         dominantLangPromise.then(function(language) {
@@ -28,7 +28,7 @@ exports.handler = function(event, context, callback) {
             // AWS Comprehend - Detect Entities
             var detectEntitiesParams = {
                 LanguageCode: languageCode,
-                Text: payload.Message__c
+                Text: payload.detail.Message__c
             };
             var detectEntitiesPromise = comprehend.detectEntities(detectEntitiesParams).promise();
             
@@ -39,7 +39,7 @@ exports.handler = function(event, context, callback) {
 
                 // Salesforce - Update Lead 
                 var lead = nforce.createSObject('Lead');
-                lead.set('Id', payload.Record_Id__c);
+                lead.set('Id', payload.detail.Record_Id__c);
                 lead.set('Language__c', language.Languages[0].LanguageCode);
                 lead.set('Language_Accuracy__c', language.Languages[0].Score * 100);
                 lead.set('Response__c', JSON.stringify(language));
@@ -49,7 +49,7 @@ exports.handler = function(event, context, callback) {
                 });
                 
                 // Salesforce - Delete existing Lead Entities
-                var q = 'SELECT Id FROM Lead_Entity__c WHERE Lead__c = \'' + payload.Record_Id__c + '\' LIMIT 200';
+                var q = 'SELECT Id FROM Lead_Entity__c WHERE Lead__c = \'' + payload.detail.Record_Id__c + '\' LIMIT 200';
                 org.query({ query: q }, function(err, resp){
                     
                   if(!err && resp.records) {
@@ -58,7 +58,7 @@ exports.handler = function(event, context, callback) {
                         if(deleteIds) deleteIds += ',';
                         deleteIds += JSON.parse(JSON.stringify(oldRecord)).id;
                     }
-                    org.deleteUrl( { url: '/services/data/v48.0/composite/sobjects?ids=' + deleteIds } , function(err, resp){
+                    org.deleteUrl( { url: '/services/data/v52.0/composite/sobjects?ids=' + deleteIds } , function(err, resp){
                         if(err) console.log(err, err.stack); // an error occurred
                     });
                   }
@@ -68,14 +68,14 @@ exports.handler = function(event, context, callback) {
                 for (var entity of data[0].Entities) {
                     var leadEntity = nforce.createSObject('Lead_Entity__c');
                     leadEntity.set('attributes', { 'type' : 'Lead_Entity__c' } );
-                    leadEntity.set('Lead__c', payload.Record_Id__c);
+                    leadEntity.set('Lead__c', payload.detail.Record_Id__c);
                     leadEntity.set('Accuracy__c', entity.Score * 100);
                     leadEntity.set('Type__c', entity.Type);
                     leadEntity.set('Text__c', entity.Text);
                     leadEntity.set('Name', entity.Type + ' ' + entity.Text);
                     leadEntities.push(leadEntity);
                 }
-                org.postUrl( { url: '/services/data/v48.0/composite/sobjects/' , body : { records : leadEntities } } , function(err, resp){
+                org.postUrl( { url: '/services/data/v52.0/composite/sobjects/' , body : { records : leadEntities } } , function(err, resp){
                     if(err) console.log(err, err.stack); // an error occurred
                 });
                 
